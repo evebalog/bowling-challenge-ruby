a4c5e715f71f243ff0ba60337590abc47163374d
Eva Balog <evebalog@netscape.net>
2017-09-10 11:49:36 +0200

    Refactor tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,13 @@
 require 'game'
 
 describe Game do
+  let(:game) {Game.new}
   it 'scores zero in a guttergame' do
-    game = Game.new
     game.pins([0] * 20)
     expect(game.score).to eq 0
   end
 
   it 'scores 40 in the game' do
-    game = Game.new
     game.pins([2] * 20)
     expect(game.score).to eq 40
   end
