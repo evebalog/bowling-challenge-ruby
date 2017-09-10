6ce0b2b63684c54e1faec1c8b1a741a7584da407
Eva Balog <evebalog@netscape.net>
2017-09-10 12:24:24 +0200

    Refactor spare and standard score

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,11 @@
 class Game
 
+  attr_reader :first_roll_in_frame
+
+  def initialize
+    @first_roll_in_frame = 0
+  end
+
   def pins(rolls)
     @rolls = rolls
   end
@@ -7,20 +13,33 @@ class Game
   def score
     frame = 0
     score = 0
-    first_roll_in_frame = 0
 
     while frame < 10
-      if @rolls[first_roll_in_frame] + @rolls[first_roll_in_frame + 1] == 10
-        score += 10 + @rolls[first_roll_in_frame + 2]
+      if spare?
+        score += 10 + bonus_for_spare
       else
-        score += @rolls[first_roll_in_frame] + @rolls[first_roll_in_frame + 1]
+        score += standard_score
       end
 
       frame += 1
-      first_roll_in_frame += 2
+      @first_roll_in_frame += 2
     end
 
     score
+  end
+
+  private
+
+  def spare?
+    @rolls[first_roll_in_frame] + @rolls[first_roll_in_frame + 1] == 10
+  end
+
+  def bonus_for_spare
+    @rolls[first_roll_in_frame + 2]
+  end
+
+  def standard_score
+    @rolls[first_roll_in_frame] + @rolls[first_roll_in_frame + 1]
   end
 
 end
